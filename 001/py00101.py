adab8a4f0ef7960038aabc173577281e982feea8
wforst-breeze <breezeblz@hotmail.com>
2023-06-23 21:32:45 +0800

    添加注释

--- 001/py00101.py
+++ 001/py00101.py
@@ -1,4 +1,7 @@
+# print 可以使应用输出一段指定的字符串
 print("Hello world!")
+# 加号可以连接多个字符串，但不包含空格
 print("Also"+" try"+" Java"+"!")
+# 当字符串内需要使用引号时，可以在内外使用不同的引号或使用 “\” 转义
 print('They call it "Jazz"!')
 print('He said: \"let\'s Go\"')
