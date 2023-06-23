bcfe50229e810ce7468c978e6aead2fa4eef025b
wforst-breeze <breezeblz@hotmail.com>
2023-06-23 21:36:48 +0800

    学会换行

--- 001/py00101.py
+++ 001/py00101.py
@@ -5,3 +5,14 @@ print("Also"+" try"+" Java"+"!")
 # 当字符串内需要使用引号时，可以在内外使用不同的引号或使用 “\” 转义
 print('They call it "Jazz"!')
 print('He said: \"let\'s Go\"')
+# “\n” 可以使得输出的内容换行
+print("Line 1\nAnd... Line 2!")
+# 懒狗版：使用三引号 '''text''' """text""" 可以进行懒狗换行！
+print('''To see a world in a grain of sand,
+一粒沙里阅世界
+And a heaven in a wild flower,
+一朵花中觅天堂
+Hold infinity in the palm of your hand,
+无穷尽在掌心中
+And eternity in an hour.
+永恒不过一刻钟''')
