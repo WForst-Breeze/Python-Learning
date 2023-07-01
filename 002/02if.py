8dd2c5937ef966eb2103d16b28bd429c651b558c
wforst-breeze <breezeblz@hotmail.com>
2023-07-01 12:03:47 +0800

    石剑是检验真理的唯一标准

--- 002/02if.py
+++ 002/02if.py
@@ -29,3 +29,42 @@ else:
         print("错误：光照等级不得低于 0")  # 抛出错误
     else:
         print("此处会生成敌对怪物")  # 输出结论
+
+print("==========")
+
+# 如果你想简洁化下面这串玩意：
+guess = int(input("猜猜我心里想的数字吧："))
+
+if guess == 86:
+    print("耶！你猜对了！")
+else:
+    if guess > 86:  # 再次判断大小并给出提示
+        print("我想的数字比这个小")
+    else:
+        print("我想的数字比这个大")
+
+print("==========")
+
+# 你可以使用 elif 来简化这个金字塔一样的玩意
+guess = int(input("猜猜我心里想的数字吧："))
+if guess == 86:
+    print("耶！你猜对了！")
+elif guess > 86:  # elif=else if
+    print("我想的数字比这个小")
+else:
+    print("我想的数字比这个大")
+
+print("==========")
+
+# 接下来我们来做一个 BMI 判断的程序，先把之前做的 BMI 计算代码抄过来
+height = float(input("您的身高是（单位：米）："))  # 用户输入身高
+weight = float(input("您的体重是（单位：千克）："))  # 用户输入体重
+bmi = float(weight / (height ** 2))    # 计算用户 bmi，由于身高和体重可能出现小数，因此转换为浮点型进行运算，否则可能会报错
+if bmi <= 18.5:141
+    print("您的 BMI 值为："+str(bmi)+"，属于偏瘦范围")
+elif 18.5 < bmi <= 25:
+    print("您的 BMI 值为：" + str(bmi) + "，属于正常范围")
+elif 25 < bmi <= 30:
+    print("您的 BMI 值为：" + str(bmi) + "，属于偏胖范围")
+else:
+    print("您的 BMI 值为：" + str(bmi) + "，属于肥胖范围")
